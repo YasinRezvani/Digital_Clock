9fcea43f17a74db96dba140fc43c8c18c5fcc8a0
YasinRezvani <yasinrezvani2025@gmail.com>
2021-08-21 15:15:39 +0430

    create root and set Tk()

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 from tkinter import *
 from time import strftime
+
+root = Tk()
+root.title("Digital Clock")
+root.resizable(False, False)
+
+def time():
+    string = strftime('%H:%M:%S %p\n %d-%m-%Y ')
+    label.config(text = string)
+    label.after(1000,time)
+
+label = Label(root , font=("Arial",35) , background = "black" , foreground="cyan")
+label.pack(anchor="center")
+time()
+
+mainloop()
+
+
+# Made By Yasin Rezvani
